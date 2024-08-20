107aadae44cc93699c354ebcd537de3e937ce478
zsheperdigian <zsheperdigian@gmail.com>
2024-08-20 10:44:04 -0700

    adding all the stuff i've been working on for nextjs

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+
+export const lusitana = Lusitana({ 
+    weight: "400", 
+    subsets: ['latin'],
+});
+export const inter = Inter({ subsets: ['latin']});
+
